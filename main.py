4c24d2adc2b367882b588897b199ca4b49f712d6
Neneg2005 <137720020+Neneg2005@users.noreply.github.com>
2023-09-21 20:48:37 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+print("Hello! I am a band name generator.")
+
+city_name = input("What city are you from?\n")
+
+pet_name = input("What is your pet called?\n")
+
+print("Your band name is: " + city_name + " " + pet_name)
+
+
