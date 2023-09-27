d862f3332e310b82a52b3698b136af5208a2d92e
Neneg2005 <137720020+Neneg2005@users.noreply.github.com>
2023-09-27 17:07:28 +0100

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -26,38 +26,32 @@ scissors = '''
 ---.__(___)
 '''
 
-#Write your code below this line 👇
 
 #You
 rps = [rock, paper, scissors]
 you_chose = int(input("Type 0 for rock, 1 for paper and 2 for scissors. "))
-your_choice = rps[you_chose]
-printed_your_choice = print(f"You chose: {your_choice}")
+if you_chose >= 3 or you_chose < 0:
+  print("You chose an invalid number. You lose!")
+else:
+  your_choice = rps[you_chose]
+  printed_your_choice = print(f"You chose: {your_choice}")
 
 #Comp
-random_rps = random.randint(0, 2)
-comp_choice = rps[random_rps]
-printed_comp_choice = print(f"Computer chose: {comp_choice}")
+  random_rps = random.randint(0, 2)
+  comp_choice = rps[random_rps]
+  printed_comp_choice = print(f"Computer chose: {comp_choice}")
 
-if your_choice == comp_choice:
-  print("Draw!")
-elif your_choice == 0 and comp_choice == 1:
-  print("Computer wins!")
-elif your_choice == 0 and comp_choice == 2:
-  print("You win!")
-elif your_choice == 1 and comp_choice == 0:
-  print("You wins!")
-elif your_choice == 1 and comp_choice == 2:
-  print("Computer wins!")
-elif your_choice == 2 and comp_choice == 0:
-  print("Computer wins!")
-else:
-  print("You win!")
-
-
-
-
-
-
-
-
+  if your_choice == comp_choice:
+    print("Draw!")
+  elif your_choice == 0 and comp_choice == 1:
+    print("Computer wins!")
+  elif your_choice == 0 and comp_choice == 2:
+    print("You win!")
+  elif your_choice == 1 and comp_choice == 0:
+    print("You wins!")
+  elif your_choice == 1 and comp_choice == 2:
+    print("Computer wins!")
+  elif your_choice == 2 and comp_choice == 0:
+    print("Computer wins!")
+  else:
+    print("You win!")
